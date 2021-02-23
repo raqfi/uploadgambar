ec5ff9a3cdd8cac7a8587e8bc060c1c53ffb266a
raqfi <79461020+raqfi@users.noreply.github.com>
2021-02-23 15:21:29 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,115 @@
+<?php 
+session_start();
+if(! isset($_SESSION['is_login']))
+{
+  header('location:login.php');
+}
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <title>Starter Template · Bootstrap</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/starter-template/">
+
+    <!-- Bootstrap core CSS -->
+<link href="assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="assets/css/starter-template.css" rel="stylesheet">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+  <a class="navbar-brand" href="#">TRASHURE</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+    
+
+    <a href="logout.php" class="form-inline my-2 my-lg-0 btn btn-secondary">Logout</a>
+
+  </div>
+</nav>
+
+<main role="main" class="container">
+
+  <div class="starter-template">
+    <h1>Selamat Datang <?php echo $_SESSION['nama']; ?></h1>
+    <p class="lead">Di Trashure</p>
+  </div>
+
+</main><!-- /.container -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script>window.jQuery || document.write('<script src="/docs/4.3/assets/js/vendor/jquery-slim.min.js"><\/script>')</script><script src="/docs/4.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script></body>
+</html>
+<html>
+<head>
+	<title>Data Gambar Trashure</title>
+</head>
+<body>
+<h1>Data Gambar</h1><hr>
+<a href="form.php">Tambah Gambar</a><br><br>
+
+<table border="1" cellpadding="8">
+<tr>
+	<th>Gambar</th>
+	<th>Nama File</th>
+	<th>Ukuran File</th>
+	<th>Tipe File</th>
+	<th>Jumlah Poin</th>
+</tr>
+
+<?php
+// Load file koneksi.php
+include "koneksi.php";
+
+$query = "SELECT * FROM gambar"; // Tampilkan semua data gambar
+$sql = mysqli_query($connect, $query); // Eksekusi/Jalankan query dari variabel $query
+$row = mysqli_num_rows($sql); // Ambil jumlah data dari hasil eksekusi $sql
+
+if($row > 0){ // Jika jumlah data lebih dari 0 (Berarti jika data ada)
+	while($data = mysqli_fetch_array($sql)){ // Ambil semua data dari hasil eksekusi $sql
+		echo "<tr>";
+		echo "<td><img src='images/".$data['nama']."' width='100' height='100'></td>";
+		echo "<td>".$data['nama']."</td>";
+		echo "<td>".$data['ukuran']."</td>";
+		echo "<td>".$data['tipe']."</td>";
+		
+		echo "</tr>";
+	}
+}else{ // Jika data tidak ada
+	echo "<tr><td colspan='4'>Data tidak ada</td></tr>";
+}
+?>
+</table>
+</body>
+</html>
